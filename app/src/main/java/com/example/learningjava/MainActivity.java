5b79e97954717abec0cc37c28b2e28385c959f5d
N3xou <47154462+N3xou@users.noreply.github.com>
2024-03-10 13:44:38 +0100

    Init

---
+++ app/src/main/java/com/example/learningjava/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.learningjava;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.Button;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void disable(View v )
+    {
+        v.setEnabled(false);
+        Button vbutton = (Button) v;
+        vbutton.setText("Test");
+
+        if (v.getId() == R.id.button2)
+        {
+            handleText(findViewById(R.id.inputText));
+            findViewById(R.id.button3).setEnabled(true);
+            ((Button) findViewById(R.id.button3)).setText("enabled");
+            Log.d("success","b3 enabled");
+        }
+        else {
+            handleText(findViewById(R.id.inputText));
+            findViewById(R.id.button2).setEnabled(true);
+            ((Button) findViewById(R.id.button2)).setText("enabled");
+            Log.d("success","b2 enabled");
+        }
+
+    }
+    public void handleText(View v)
+    {
+        String txt = ((EditText) v).getText().toString();
+        Log.d("text:", txt);
+        Toast.makeText(this,"TestAlert",Toast.LENGTH_LONG).show();
+        ((TextView) findViewById(R.id.hello)).setText(txt);
+    }
+
+}
