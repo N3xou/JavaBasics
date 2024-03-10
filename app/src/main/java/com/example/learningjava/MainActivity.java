796bf56fc8c5f6ffa0f5f4ec9ba2790d28ebf249
N3xou <47154462+N3xou@users.noreply.github.com>
2024-03-10 16:01:32 +0100

    up

--- app/src/main/java/com/example/learningjava/MainActivity.java
+++ app/src/main/java/com/example/learningjava/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.learningjava;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        setTitle("Home");
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -31,18 +33,19 @@ public class MainActivity extends AppCompatActivity {
     {
         v.setEnabled(false);
         Button vbutton = (Button) v;
-        vbutton.setText("Test");
 
         if (v.getId() == R.id.button2)
         {
             handleText(findViewById(R.id.inputText));
             findViewById(R.id.button3).setEnabled(true);
+            vbutton.setText("disabled");
             ((Button) findViewById(R.id.button3)).setText("enabled");
             Log.d("success","b3 enabled");
         }
         else {
             handleText(findViewById(R.id.inputText));
             findViewById(R.id.button2).setEnabled(true);
+            vbutton.setText("disabled");
             ((Button) findViewById(R.id.button2)).setText("enabled");
             Log.d("success","b2 enabled");
         }
@@ -55,5 +58,9 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this,"TestAlert",Toast.LENGTH_LONG).show();
         ((TextView) findViewById(R.id.hello)).setText(txt);
     }
-
+    public void launchAct2(View v)
+    {
+        Intent i = new Intent(this, MainActivity2.class);
+        startActivity(i);
+    }
 }
